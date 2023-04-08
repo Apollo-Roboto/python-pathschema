a7af2d07946c72700211be84b6d63c7caa092d36
Apollo-Roboto <41392253+Apollo-Roboto@users.noreply.github.com>
2023-04-08 16:09:34 -0400

    progress

--- src/fss.py
+++ src/fss.py
@@ -2,23 +2,23 @@ from dataclasses import dataclass
 from typing import Union, Optional
 from pathlib import Path
 
-sample_schema = {
-	"root/": {
-		"test.exe": None,
-		"a_file.txt": None,
-		"a_directory/": {
-			
-		}
-	}
-}
+sample_schema = """
+root/
+	text.exe
+	a_file.txt
+	a_directory/
+"""
 
-def validate(path: Union[str, Path]):
+def validate(path: Union[str, Path], schema: str):
 	if(isinstance(path, str)):
 		path = Path(path)
-	
 
 @dataclass
 class fssFile:
+	name: str
 	parent: Optional['fssFile']
 	childs: list['fssFile']
-	
+
+	@property
+	def is_dir(self):
+		return self.name.endswith('/')
--- tests/test_validate.py
+++ tests/test_validate.py
@@ -1,15 +1,24 @@
 import unittest
-import src.fss
+from src import fss
 import tempfile
+import uuid
+from pathlib import Path
 
 class TestValidate(unittest.TestCase):
 
+	def _temp_dir(self):
+		return Path(
+			tempfile.gettempdir(),
+			str(uuid.uuid4())
+		)
+
 	def test_validate(self):
 		
-		# C:\Users\USER\AppData\Local\Temp
-		dir = tempfile.gettempdir()
+		dir = self._temp_dir()
 
 		print(dir)
 
-		# fss.validate()
+		schema = "root/"
+
+		fss.validate(path=dir, schema=schema)
 		# self.assertEqual('foo'.upper(), 'FOO')
