236d0f9d802a331ce91df189ac80200890705d95
Apollo-Roboto <41392253+Apollo-Roboto@users.noreply.github.com>
2023-06-05 21:50:23 -0400

    Added support for different indentation (#19)
    * custom indentation seems to work
    
    * updated test schema to use spaces for readability
    
    * better indentation check

--- pathschema/parser.py
+++ pathschema/parser.py
@@ -16,28 +16,64 @@ class Parser():
 	any_token = '...'
 	comment_token = '#'
 
-	def _detect_indentation(self):
-		raise NotImplementedError()
+	def _detect_indentation(self, schema: str) -> str:
+		for line in schema.split('\n'):
 
-	def _indentation_count(self,  string: str):
-		num_of_indentation = 0
-		for character in string:
-			if character == self.indentation_token:
-				num_of_indentation += 1
-			else:
-				break
+			# ignore empty lines
+			if not line.strip():
+				continue
+			
+			leading_whitespace = len(line) - len(line.lstrip())
 
-		return num_of_indentation
+			# impossible to evaluate if no leading whitespace, continue to next line
+			if leading_whitespace == 0:
+				continue
+			
+			# indentation is tabs
+			if line.startswith('\t'):
+				return '\t' * leading_whitespace
+			
+			# indentation is spaces
+			if line.startswith(' '):
+				return ' ' * leading_whitespace
+		
+		# if not evaluated, assume default
+		return self.indentation_token
+
+	def _indentation_count(self,  string: str, line_num: int) -> int:
+		"""Counts the indentation at the beginning of a string using the indentation token"""
+		tmp_str = string
+		count = 0
+		
+		while tmp_str.startswith(self.indentation_token):
+			count += 1
+			tmp_str = tmp_str[len(self.indentation_token):]
+
+		# if there is still some whitespace
+		# can happen if indentation token is 4 space and there are 3 spaces instead
+		if len(tmp_str) > len(tmp_str.lstrip()):
+			raise SchemaError('Inconsistent indentation.', line_num)
+		
+		return count
 
 	def schema_to_node_tree(self, schema: str) -> DirPathNode:
+
+		self.indentation_token = self._detect_indentation(schema)
 
 		root_node: DirPathNode = DirPathNode(name='schema_root')
 
 		current_node = root_node
 		node_depth = 0
+		last_indentation = 0
 
 		for line_num, line in enumerate(schema.split('\n')):
-			indentation = self._indentation_count(line)
+			indentation = self._indentation_count(line, line_num+1)
+
+			if indentation > last_indentation + 1:
+				raise SchemaError('Inconsistent indentation.', line_num+1)
+
+			last_indentation = indentation
+			
 			name = line.strip()
 			if len(name) == 0:
 				continue
